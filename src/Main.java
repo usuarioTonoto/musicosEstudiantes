1155d76aed7675f63118948bb20ed7f99d9ae061
Megiddo <juancholo182005@gmail.com>
2024-03-02 21:26:19 -0600

    Haciendo la clase enum

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hola mundo!");
+        for (Instrumento i : Instrumento.values()){
+            System.out.println(i);
+        }
     }
 }
