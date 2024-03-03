8d6167557f62c0b137ccc0db5752cde732fcb7a5
Usuario Tonoto <usuariotonoto@gmail.com>
2024-03-02 21:41:47 -0600

    feat(Musico): agregando la interfaz de Musico

---
+++ src/Musico.java
@@ -0,0 +1,19 @@
+public interface Musico {
+    default public void tocarInstrumento(Instrumento i) {
+        switch (i) {
+            case PIANO:
+                System.out.println("Tocando el piano");
+                break;
+            case TECLADO:
+                System.out.println("Tocando el teclado");
+                break;
+            case TROMPETA:
+                System.out.println("Brrrum brrrum brrrrumm buuu");
+                break;
+            case GUITARRA:
+                System.out.println("Es la guitarra de Lolo");
+                break;
+        }
+    }
+    public String getNombreArtistico();
+}
